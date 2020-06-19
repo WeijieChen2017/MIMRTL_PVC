0676d24bf36daa60fa64ed4532b7ea56f2b94484
WeijieChen2017 <weijiechen1994@gmail.com>
2020-06-19 00:19:05 -0500

    v1

--- S1_simulation.py
+++ S1_simulation.py
@@ -80,7 +80,7 @@ def nib_smooth(file_mri, data, fwhm, tag, save_path):
     smoothed_file = nibabel.Nifti1Image(smoothed_data, file_mri.affine, file_mri.header)
 #     print(np.amax(smoothed_file.get_fdata()))
     nibabel.save(smoothed_file, save_path+"fwhm_"+str(fwhm)+"_"+tag+".nii")
-    print("fwhm_"+str(idx_fwhm)+"_"+tag+".nii")
+    print("fwhm_"+str(fwhm)+"_"+tag+".nii")
 
 def sythesize_data(name_dataset='sk8R', fwhm_hub=[8], 
                    gau_sigma_hub=[], poi_sigma_hub=[], flag_Radon=False, theta=[]):
