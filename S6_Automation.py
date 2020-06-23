f5a2e4cd406c4055ff22c9311cdb69d4c0e2d1e8
WeijieChen2017 <weijiechen1994@gmail.com>
2020-06-23 04:33:56 -0500

    v1

--- S6_Automation.py
+++ S6_Automation.py
@@ -20,29 +20,29 @@ def main():
         command_0 = "python S0_reverse.py --nameDataset " + name
         command_0 += " --powerFactor 1"
         print(command_0)
-        # os.system(command_0)
+        os.system(command_0)
 
         command_1 = "python S1_simulation.py --nameDataset " + name
         command_1 += " --fwhmHub 8 --gauHub 0 --poiHub 0"
         print(command_1)
-        # os.system(command_1)
+        os.system(command_1)
 
         command_2 = "python S2_createDataset.py --nameDataset "+name
         command_2 += " --outputChannel 1"
         print(command_2)
-        # os.system(command_2)
+        os.system(command_2)
 
         command_3 = "python S3_train.py --nameDataset "+name
         print(command_3)
-        # os.system(command_3)
+        os.system(command_3)
 
         command_4 = "python S4_test.py --nameDataset "+name
         print(command_4)
-        # os.system(command_4)
+        os.system(command_4)
 
         command_5 = "python S5_assembler.py --nameDataset "+name
         print(command_5)
-        # os.system(command_5)
+        os.system(command_5)
         
     # command_0 = "pytorch-CycleGAN-and-pix2pix"
     # print("------------------------")
