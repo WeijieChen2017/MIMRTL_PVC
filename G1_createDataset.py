e68bd0b69989bfb708635e17e1ea4772fc8c8c7b
WeijieChen2017 <weijiechen1994@gmail.com>
2020-12-23 02:07:40 -0600

    Matthew and Tim

--- G1_createDataset.py
+++ G1_createDataset.py
@@ -27,98 +27,69 @@ def main():
     tmpl_header = tmpl_nii.header
     tmpl_affine = tmpl_nii.affine
 
-    for folder_name in ["gt", "gt_f3", "blur", "test", "pure"]:
+    for folder_name in ["test_gt", "test_f3", "test_recon", "train_recon", "train_gt", "train_f3"]:
             path = "./data/"+name_dataset+"/"+folder_name+"/"
             if not os.path.exists(path):
                 os.makedirs(path)
 
-    key_list = ["test"]
-    if not flag_test_only:
-        key_list.append("t1")
-        key_list.append("recon")
+    if flag_test_only:
+        tag_list = ["test"]
+    else:
+        tag_list = ["test", "train"]
+    key_list = ["recon", "t1"]
         
-    for mat_key in key_list:
-        mat_list = glob.glob("./nifty/"+name_dataset+"/"+mat_key+"/*.mat")
-        mat_list.sort()
-        for mat_name in mat_list:
-            print("-----------------------------------------------")
-            mdict = loadmat(mat_name)
+    for mat_tag in tag_list:
+        for mat_key in key_list:
+            tag_key = mat_tag+"_"+mat_key
+            mat_list = glob.glob("./nifty/"+name_dataset+"/"+tag_key+"/*.mat")
+            mat_list.sort()
+            for mat_name in mat_list:
+                print("-----------------------------------------------")
+                mdict = loadmat(mat_name)
 
-            try:
-                mat_data = mdict["reconImg"]
-            except Exception:
-                pass  # or you could use 'continue'
+                try:
+                    mat_data = mdict["reconImg"]
+                except Exception:
+                    pass  # or you could use 'continue'
 
-            try:
-                mat_data = mdict["data"]
-            except Exception:
-                pass  # or you could use 'continue'
+                try:
+                    mat_data = mdict["data"]
+                except Exception:
+                    pass  # or you could use 'continue'
 
-        #     print(mat_data.shape)
-            if mat_key == "t1":
-                process_data_t1_DL(mat_data, name_dataset)
-            if mat_key == "recon":
-                process_data_recon_DL(mat_data, name_dataset)
-            if mat_key == "test":
-                process_data_test_DL(mat_data, name_dataset)
+                process_data_t1_DL(data=mat_data, name_dataset=name_dataset,
+                                   mat_tag=mat_tag, mat_key=mat_key, tmpl_header, tmpl_affine)
 
+def process_data_DL(mat_data, name_dataset, mat_tag, mat_key, tmpl_header, tmpl_affine):
 
+    if tag == "recon":
+        print("Data dim:", mat_data.shape)
+        save_name = "./data/"+name_dataset+"/"+mat_tag+"_"+mat_key"/"+os.path.basename(mat_name)[:20]+".nii"
+        save_file = nib.Nifti1Image(data, affine=tmpl_affine, header=tmpl_header)
+        nib.save(save_file, save_name)
+        print(save_name)
 
-def process_data_recon_DL(data, name_dataset):
-    zoom_data = data
-    print("Old dim:", data.shape)
-    print("New dim:", zoom_data.shape)
-    
-    save_file = nib.Nifti1Image(zoom_data, affine=tmpl_affine, header=tmpl_header)
-    save_name = "./data/"+name_dataset+"/blur/"+os.path.basename(mat_name)[:20]+".nii"
-    nib.save(save_file, save_name)
-    print(save_name)
+    if tag == "t1":
+        px, py, pz = mat_data.shape
+        qx, qy, qz = (256, 256, 56)
+        zoom_data = zoom(mat_data, (qx/px, qy/py, qz/pz))
+        standard_pet = np.zeros((256, 256, 89))
+        standard_pet[:, :, 17:73] = zoom_data
+        standard_pet[standard_pet<0] = 0
 
-def process_data_t1_DL(data, name_dataset):
-    px, py, pz = data.shape
-    qx, qy, qz = (256, 256, 56)
-    zoom_data = zoom(data, (qx/px, qy/py, qz/pz))
-    standard_pet = np.zeros((256, 256, 89))
-    standard_pet[:, :, 17:73] = zoom_data
-    standard_pet[standard_pet<0] = 0
+        print("Old dim:", mat_data.shape)
+        print("New dim:", standard_pet.shape)
+        
+        save_file = nib.Nifti1Image(standard_pet, affine=tmpl_affine, header=tmpl_header)
+        save_name = "./data/"+name_dataset+"/"+mat_tag+"_gt/"+os.path.basename(mat_name)[:20]+".nii"
+        nib.save(save_file, save_name)
+        print(save_name)
 
-    print("Old dim:", data.shape)
-    print("New dim:", standard_pet.shape)
-    
-    save_file = nib.Nifti1Image(standard_pet, affine=tmpl_affine, header=tmpl_header)  
-    smoothed_file = processing.smooth_image(save_file, fwhm=3, mode='nearest')
-    save_name = "./data/"+name_dataset+"/pure/"+os.path.basename(mat_name)[:20]+".nii"
-    nib.save(smoothed_file, save_name)
-    print(save_name)
-
-
-def process_data_test_DL(data, name_dataset):
-
-    zoom_data = data   
-    save_file = nib.Nifti1Image(zoom_data, affine=tmpl_affine, header=tmpl_header)
-    save_name = "./data/"+name_dataset+"/test/"+os.path.basename(mat_name)[:20]+".nii"
-    nib.save(save_file, save_name)
-    print(save_name)
-
-    px, py, pz = data.shape
-    qx, qy, qz = (256, 256, 56)
-    zoom_data = zoom(data, (qx/px, qy/py, qz/pz))
-    standard_pet = np.zeros((256, 256, 89))
-    standard_pet[:, :, 17:73] = zoom_data
-    standard_pet[standard_pet<0] = 0
-
-    print("Old dim:", data.shape)
-    print("New dim:", standard_pet.shape)
-    
-    save_file = nib.Nifti1Image(standard_pet, affine=tmpl_affine, header=tmpl_header)
-    save_name = "./data/"+name_dataset+"/gt/"+os.path.basename(mat_name)[:20]+".nii"
-    nib.save(save_file, save_name)
-    print(save_name)
-
-    smoothed_file = processing.smooth_image(save_file, fwhm=3, mode='nearest')
-    save_name = "./data/"+name_dataset+"/gt_f3/"+os.path.basename(mat_name)[:20]+".nii"
-    nib.save(smoothed_file, save_name)
-    print(save_name)
+        smoothed_file = processing.smooth_image(save_file, fwhm=3, mode='nearest')
+        save_name = "./data/"+name_dataset+"/"+mat_tag+"_f3/"+os.path.basename(mat_name)[:20]+".nii"
+        save_nifty(smoothed_file.get_fdata(), name, tmpl_header, tmpl_affine)
+        nib.save(save_file, save_name)
+        print(save_name)
 
 if __name__ == "__main__":
     main()
